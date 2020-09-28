86dd001d678085c91e3420fdb1ea3cc57c693777
Fauzan <muhdfauzan9302@gmail.com>
2020-09-28 11:50:22 +0800

    :wrench: Fix jwt algo not found

--- src/api/middlewares/isAuth.ts
+++ src/api/middlewares/isAuth.ts
@@ -26,6 +26,7 @@ const getTokenFromHeader = req => {
 
 const isAuth = jwt({
   secret: config.jwtSecret, // The _secret_ to sign the JWTs
+  algorithms: [config.jwtAlgorithm], // JWT Algorithm
   userProperty: 'token', // Use req.token to store the JWT
   getToken: getTokenFromHeader, // How to extract the JWT from the request
 });
